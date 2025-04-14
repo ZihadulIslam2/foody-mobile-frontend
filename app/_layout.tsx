2eedfed4fdaecfeb08ee678a2173af32a9a3ee82
zihadulislam2 <zihadul708@gmail.com>
2025-04-14 07:20:26 +0600

    Update app name, layout structure, and dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return(
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
-
+import { Text, View } from 'react-native';
+import '../global.css';
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text className="font-bold text-3xl">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
